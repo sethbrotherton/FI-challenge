2081b81d45f8714258679de3d4489f023e2305e0
Seth - home computer <sethbrotherton@yahoo.com>
2018-11-18 02:12:17 -0500

    Initial commit of challenge for FI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TextArea from "./TextArea";
+import fractalIndustries from "./Fractal-Industries.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={fractalIndustries} className="App-logo" alt="logo" />
+          <p>Thank you for challenging me :-/</p>
+          <p>Test your syntax here!</p>
+          <TextArea />
         </header>
       </div>
     );
---
+++ src/TextArea.js
@@ -0,0 +1,201 @@
+import React from "react";
+import "./TextArea.css";
+
+class TextArea extends React.Component {
+  state = {
+    value: "",
+    braceError: false,
+    squareError: false,
+    parenthError: false,
+    closeCurlyFirst: false,
+    closeParenthFirst: false,
+    closeSquareFirst: false
+  };
+
+  handleInput = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  handleSyntax = e => {
+    this.handleParenth();
+    this.handleCurlyBraces();
+    this.handleSquareBrackets();
+    this.closingCurlyFirst();
+    this.closingParenthFirst();
+    this.closingSquareFirst();
+  };
+
+  handleCurlyBraces = () => {
+    const { value } = this.state;
+    const valueArray = value.split("");
+    const openBraces = valueArray.filter(char => char === "{");
+    const closeBraces = valueArray.filter(char => char === "}");
+    if (openBraces.length !== closeBraces.length) {
+      this.setState({
+        braceError: true
+      });
+    } else if (openBraces.length === closeBraces.length) {
+      this.setState({
+        braceError: false
+      });
+    }
+  };
+
+  handleParenth = () => {
+    const { value } = this.state;
+    const valueArray = value.split("");
+    const openParenth = valueArray.filter(char => char === "(");
+    const closeParenth = valueArray.filter(char => char === ")");
+    if (openParenth.length !== closeParenth.length) {
+      this.setState({
+        parenthError: true
+      });
+    } else if (openParenth.length === closeParenth.length) {
+      this.setState({
+        parenthError: false
+      });
+    }
+  };
+
+  handleSquareBrackets = () => {
+    const { value } = this.state;
+    const valueArray = value.split("");
+    const openSquare = valueArray.filter(char => char === "[");
+    const closeSquare = valueArray.filter(char => char === "]");
+    if (openSquare.length !== closeSquare.length) {
+      this.setState({
+        squareError: true
+      });
+    } else if (openSquare.length === closeSquare.length) {
+      this.setState({
+        squareError: false
+      });
+    }
+  };
+
+  closingCurlyFirst = () => {
+    let curly = 0;
+    const { value } = this.state;
+    const valueArray = value.split("");
+    valueArray.forEach(char => {
+      if (char === "}") {
+        curly -= 1;
+        if (curly <= -1) {
+          return this.setState({
+            closeCurlyFirst: true
+          });
+        } else {
+          this.setState({
+            closeCurlyFirst: false
+          });
+        }
+      } else if (char === "{") {
+        curly += 1;
+      }
+      return curly;
+    });
+  };
+
+  closingParenthFirst = () => {
+    let parenth = 0;
+    const { value } = this.state;
+    const valueArray = value.split("");
+    valueArray.forEach(char => {
+      if (char === ")") {
+        parenth -= 1;
+        if (parenth <= -1) {
+          return this.setState({
+            closeParenthFirst: true
+          });
+        } else {
+          this.setState({
+            closeParenthFirst: false
+          });
+        }
+      } else if (char === "(") {
+        parenth += 1;
+      }
+      return parenth;
+    });
+  };
+
+  closingSquareFirst = () => {
+    let square = 0;
+    const { value } = this.state;
+    const valueArray = value.split("");
+    valueArray.forEach(char => {
+      if (char === "]") {
+        square -= 1;
+        if (square <= -1) {
+          return this.setState({
+            closeSquareFirst: true
+          });
+        } else {
+          this.setState({
+            closeSquareFirst: false
+          });
+        }
+      } else if (char === "[") {
+        square += 1;
+      }
+      return square;
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <textarea
+          className="text-area"
+          rows="14"
+          cols="40"
+          value={this.state.value}
+          onChange={this.handleInput}
+          onKeyDown={this.handleSyntax}
+        />
+        <div className="errors-description">
+          {this.state.braceError ? (
+            <p className="error">Your curly braces are unbalanced</p>
+          ) : (
+            <p className="no-error">Curly braces are balanced</p>
+          )}
+          {this.state.parenthError ? (
+            <p className="error">Your parentheses are unbalanced</p>
+          ) : (
+            <p className="no-error">Parentheses are balanced</p>
+          )}
+          {this.state.squareError ? (
+            <p className="error">Your square brackets are unbalanced</p>
+          ) : (
+            <p className="no-error">Square brackets are balanced</p>
+          )}
+          {this.state.closeCurlyFirst ? (
+            <p className="error">
+              You have a closing curly brace before an opening
+            </p>
+          ) : (
+            <p className="no-error">No out of order curlies</p>
+          )}
+          {this.state.closeSquareFirst ? (
+            <p className="error">
+              You have a closing square bracket before an opening
+            </p>
+          ) : (
+            <p className="no-error">No out of order square brackets</p>
+          )}
+          {this.state.closeParenthFirst ? (
+            <p className="error">
+              You have a closing parenthesis before an opening
+            </p>
+          ) : (
+            <p className="no-error">No out of order parentheses</p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TextArea;
